e9c8dc9b8f7d9fd99275abcdce782d3d43a2d386
Taves Walker <walker.taves@gmail.com>
2019-09-20 14:34:08 -0700

    Update firstReverseTry.js

--- firstReverseTry.js
+++ firstReverseTry.js
@@ -1,5 +1,5 @@
 firstReverseTry = a =>  {
-    [a[0],a[a.length-1]]=[a[a.length-1],a[0]] 
+    [a[0],a[a.length-1]]=[a[a.length-1],a[0]] //[Array[a],Array[b]]=[Array[b],Array[a]] Swaps those values
     return a.filter(n=>n)
 }
 
