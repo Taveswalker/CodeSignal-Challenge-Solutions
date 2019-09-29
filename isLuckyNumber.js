bed4299797c70a5297a7392ab1158a66f5d4747f
Taves Walker <walker.taves@gmail.com>
2019-09-28 22:59:30 -0700

    Update isLuckyNumber.js

--- isLuckyNumber.js
+++ isLuckyNumber.js
@@ -1,3 +1,6 @@
 //top js answer is 30 chars
+//top answer:
+//return !!+` 1   2  345   6 7 8    `[Set=-~Set]
+//it was labeled 'cheese', i have no idea what it is at the moment
 
 isLuckyNumber = n => !/[01235689]/.test(n)
